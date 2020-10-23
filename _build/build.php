b94c1d4ab5238ea4f257840b388c43539c4c59b8
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-10-23 17:51:26 +0200

    Updates

--- _build/build.php
+++ _build/build.php
@@ -7,31 +7,31 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Console\Input\InputOption;
 
 (new Application('Symfony Docs Builder', '1.0'))
     ->register('build-docs')
+    ->addOption('generate-fjson-files', null, InputOption::VALUE_NONE, 'Use this option to generate docs both in HTML and JSON formats')
+    ->addOption('disable-cache', null, InputOption::VALUE_NONE, 'Use this option to force a full regeneration of all doc contents')
     ->setCode(function(InputInterface $input, OutputInterface $output) {
         $command = [
             'php',
             'vendor/symfony/docs-builder/bin/console',
             'build:docs',
-            '--output-json',
+            sprintf('--save-errors=%s', __DIR__.'/logs.txt'),
             __DIR__.'/../',
             __DIR__.'/output/',
         ];
-        $vars = [];
-/*
-        if (!$docsBuild->shouldUseCache()) {
+
+        if ($input->getOption('generate-fjson-files')) {
+            $command[] = '--output-json';
+        }
+
+        if ($input->getOption('disable-cache')) {
             $command[] = '--disable-cache';
         }
 
-        if (null !== $docsBuild->getLogsPath()) {
-            $command[] = sprintf('--save-errors=%s', $docsBuild->getLogsPath());
-        }
-*/
-        //$vars['SYMFONY_VERSION'] = '4.4';
-
-        $process = new Process($command, null, $vars);
+        $process = new Process($command);
         $process->setTimeout(3600);
         $process->run();
 
